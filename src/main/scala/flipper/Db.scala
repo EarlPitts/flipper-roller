4792356153dc0f5b15e220ee279c163c9dcb4f5e
Bendeguz Seres <grafitpor@gmail.com>
2026-08-20 07:03:54 +0200

    Refactor

--- src/main/scala/flipper/Db.scala
+++ src/main/scala/flipper/Db.scala
@@ -3,13 +3,12 @@ package flipper.db
 import cats.effect.*
 import cats.*
 import cats.implicits.*
-import com.google.cloud.firestore.{Firestore, FirestoreOptions}
+import com.google.cloud.firestore.FirestoreOptions
 import scala.jdk.CollectionConverters.*
 
 import flipper.core.Flipper.*
 import flipper.core.*
-
-case class DbConfig(projectId: String)
+import com.google.cloud.firestore.CollectionReference
 
 trait Db:
   def addFlipper(flipper: Flipper): IO[Unit]
@@ -18,7 +17,9 @@ trait Db:
   def delete(name: Name): IO[Unit]
 
 object Db:
-  def make(config: DbConfig): Resource[IO, Db] =
+  case class Config(projectId: String)
+
+  def make(config: Config): Resource[IO, Db] =
     Resource
       .fromAutoCloseable {
         IO.blocking(
@@ -31,34 +32,38 @@ object Db:
       .map { store =>
         val collection = store.collection("flippers")
         new Db {
-          def getFlippers =
-            IO.blocking(collection.get().get())
-              .map {
-                _.getDocuments.asScala.toList
-                  .map { doc =>
-                    val name = doc.getString("name") // TODO handle error
-                    val status = doc.getString("status") // TODO handle error
-                    val date = doc.getString("date") // TODO handle error
-                    val dto = FlipperDTO(name, status, date)
-                    Flipper.fromDTO(dto).get // TODO handle error
-                  }
-              }
-
-          def addFlipper(flipper: Flipper) =
-            IO.blocking(collection.add(Flipper.toDTO(flipper)).get).void
-
-          def deleteAll = IO.blocking {
-            val documents =
-              collection.get.get.getDocuments.asScala.toList
-            documents.foreach(_.getReference.delete.get())
-          }
-
-          def delete(name: Name) = IO.blocking {
-            val documents =
-              collection.get.get.getDocuments.asScala.toList
-            documents
-              .find(_.getString("name") == name.unName)
-              .map(_.getReference.delete.get)
-          }.void
+          def getFlippers = Db.getFlippers(collection)
+          def addFlipper(flipper: Flipper) = Db.addFlipper(collection)(flipper)
+          def deleteAll = Db.deleteAll(collection)
+          def delete(name: Name) = Db.delete(collection)(name)
         }
       }
+
+  def getFlippers(collection: CollectionReference) =
+    IO.blocking(collection.get().get())
+      .map {
+        _.getDocuments.asScala.toList
+          .map { doc =>
+            val name = doc.getString("name") // TODO handle error
+            val status = doc.getString("status") // TODO handle error
+            val date = doc.getString("date") // TODO handle error
+            val dto = FlipperDTO(name, status, date)
+            Flipper.fromDTO(dto).get // TODO handle error
+          }
+      }
+
+  def addFlipper(collection: CollectionReference)(flipper: Flipper) =
+    IO.blocking(collection.add(Flipper.toDTO(flipper)).get).void
+
+  def deleteAll(collection: CollectionReference) = IO.blocking {
+    val documents = collection.get.get.getDocuments.asScala.toList
+    documents.foreach(_.getReference.delete.get())
+  }
+
+  def delete(collection: CollectionReference)(name: Name) = IO.blocking {
+    val documents =
+      collection.get.get.getDocuments.asScala.toList
+    documents
+      .find(_.getString("name") == name.unName)
+      .map(_.getReference.delete.get)
+  }.void
